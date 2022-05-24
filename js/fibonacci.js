a0c44ceae79eba3ba5dbf976832a8d175cc8a064
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-24 13:41:30 -0500

    initial code

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,19 @@
 const fibonacci = (num) => {
-    
+    let result = 1;	
+    let prevResult = 0;	
+    if (num === 0){	
+      return 0;	
+    } else if (num === 1){	
+      return 1;	
+    }	
+    for (let i = 2; i <= num; i++){	
+      let tempResult = result;	
+      result = result + prevResult;	
+      prevResult = tempResult;	
+    }	
+    return result;
 }
 
 module.exports = {fibonacci}
+
+console.log(fibonacci(3))
