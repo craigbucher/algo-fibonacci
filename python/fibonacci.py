a1610a8cc8a75cd74b038372bb5cf6f8aa17334a
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-05-24 16:58:17 -0500

    first python

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,4 +1,20 @@
-def fibonacci(n):
-  pass
+def fibonacci(num):
+  if num == 0:
+    return 0
+  elif num == 1:
+    return 1
+  else:
+    prevResult = 0
+    result = 1
+    for i in range(2, num + 1):
+      tempResult = result	
+      result = result + prevResult	
+      prevResult = tempResult
+  return result
 
+# print(fibonacci(7))
 
+# def fibonacci(n):
+#      if n in {0, 1}:  # Base case
+#          return n
+#      return fibonacci(n - 1) + fibonacci(n - 2)  # Recursive case
